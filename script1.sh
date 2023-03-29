8de3e1b3fbca480f0660e38159744ede01521c81
Ebrahim Ahmed Taymour Abdelmegid, INI-DOS-TDE (EXT) <Abdelmegid.EbrahimAhmedTaymour@swisscom.com>
2023-03-30 01:44:27 +0300

    script1

---
+++ script1.sh
@@ -0,0 +1,7 @@
+source env/bin/activate
+python3 spam/trash.py  & 
+python3 trash/trash.py  &
+python3 updates/trash.py &
+python3 social/trash.py &
+python3 promotions/trash.py &
+python3 aliexpress/trash.py
